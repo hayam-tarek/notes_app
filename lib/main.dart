a7fce69edf793288ab1d8cf1c4981b69936d65bb
Hayam <hayamtareq28@gmail.com>
2024-05-29 23:17:20 +0300

    create notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -10,6 +11,13 @@ class NotesApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: false,
+        brightness: Brightness.dark,
+      ),
+      home: NotesView(),
+    );
   }
 }
