d9cc790b343933b35f3bfdef4e50275a5109f578
Hayam <hayamtareq28@gmail.com>
2024-06-18 22:20:54 +0300

    finish bottom sheet ui

--- lib/widgets/add_note_sheet.dart
+++ lib/widgets/add_note_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/custom_text_field.dart';
 
 class AddNoteSheet extends StatelessWidget {
@@ -10,22 +11,33 @@ class AddNoteSheet extends StatelessWidget {
       padding: EdgeInsets.symmetric(
         horizontal: 16,
       ),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 50,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          CustomTextField(
-            hintText: 'Content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+            ),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            CustomButton(
+              text: 'Add',
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/helper/constant.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 40,
+      child: MaterialButton(
+        textColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            8,
+          ),
+        ),
+        color: kMainColor,
+        onPressed: () {},
+        child: Text(
+          text,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
